fab339bde0740b6443404f586c32a1ea588d9cc0
John Micko <jrmicko5012@gmail.com>
2021-12-20 17:55:51 -0600

    create login form

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
+import React, { useState } from 'react';
+import Login from './Login'
 
 function App() {
+  const [id, setId] = useState()
+
+
   return (
-'hello world'
+    <>
+      {id}
+      {/* call the prop onIdSubmit because that's what happens when we submit form. It set the id as that state */}
+      <Login onIdSubmit={setId} />
+    </>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Button, Container, Form } from 'react-bootstrap'
 
-export default function Login() {
+export default function Login({onIdSubmit}) {
+
+  const idRef = useRef()
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    // this will save it in the state in the app component
+    onIdSubmit(idRef.current.value)
+  }
+
   return (
-    <div>
-      
-    </div>
+    <Container className='align-items-center d-flex' style={{height: '100vh'}}>
+      <Form onSubmit={handleSubmit} className='w-100' >
+        <Form.Group>
+          <Form.Label>Enter Your ID</Form.Label>
+          <Form.Control type="text" ref={idRef} required ></Form.Control>
+        </Form.Group>
+        <Button type='submit' className="m-2" >Login</Button>
+        <Button variant="secondary" >Create A New Id</Button>
+      </Form>
+    </Container>
   )
 }
