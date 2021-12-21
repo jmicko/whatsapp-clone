1aa0d7dac6769e7d2809f9cc078ee6f0655bf9c6
John Micko <jrmicko5012@gmail.com>
2021-12-21 11:53:11 -0600

    save id in local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
 import Login from './Login'
 
 function App() {
-  const [id, setId] = useState()
+  // custom hook that acts like useState but persists data into local storage
+  const [id, setId] = useLocalStorage('id')
 
 
   return (
